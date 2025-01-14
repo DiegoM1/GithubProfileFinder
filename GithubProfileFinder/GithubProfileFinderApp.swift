4c13a6d5a67657fa1a0ca1f9dfb0de044d5f65f5
DiegoM1 <diegomonteagudod@gmail.com>
2025-01-13 21:15:33 -0500

    Initial Commit

---
+++ GithubProfileFinder/GithubProfileFinderApp.swift
@@ -0,0 +1,20 @@
+//
+//  GithubProfileFinderApp.swift
+//  GithubProfileFinder
+//
+//  Created by Diego Monteagudo Diaz on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubProfileFinderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ GithubProfileFinderTests/GithubProfileFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubProfileFinderTests.swift
+//  GithubProfileFinderTests
+//
+//  Created by Diego Monteagudo Diaz on 13/01/25.
+//
+
+import Testing
+@testable import GithubProfileFinder
+
+struct GithubProfileFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
